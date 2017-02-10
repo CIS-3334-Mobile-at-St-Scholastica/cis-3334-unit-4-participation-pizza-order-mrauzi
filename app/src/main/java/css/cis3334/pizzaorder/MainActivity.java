a3613cd820119d058337a7a994296a2d0830630c
mrauzi <mike.rauzi@gmail.com>
2017-02-10 11:38:06 -0600

    Added spinner and order choices

--- app/src/main/java/css/cis3334/pizzaorder/MainActivity.java
+++ app/src/main/java/css/cis3334/pizzaorder/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity implements updateViewInterfa
     CheckBox chkbxDelivery;
     TextView txtTotal;
     TextView txtStatus;
+    Spinner spinnerToppings;
     PizzaOrderInterface pizzaOrderSystem;
 
     @Override
@@ -27,6 +29,8 @@ public class MainActivity extends AppCompatActivity implements updateViewInterfa
         rbSmall = (RadioButton) findViewById(R.id.radioButtonSmall);
         rbMedium = (RadioButton) findViewById(R.id.radioButtonMedium);
         rbLarge = (RadioButton) findViewById(R.id.radioButtonLarge);
+
+        spinnerToppings = (Spinner)findViewById(R.id.spinner);
 
         chkbxCheese = (CheckBox) findViewById(R.id.checkBoxCheese);
         chkbxDelivery = (CheckBox) findViewById(R.id.checkBoxDeluvery);
@@ -43,7 +47,22 @@ public class MainActivity extends AppCompatActivity implements updateViewInterfa
     }
 
     public void onClickOrder(View view) {
+        String topping = spinnerToppings.getSelectedItem().toString();
+        String size = "large";
         String orderDescription = pizzaOrderSystem.OrderPizza("Pepperoni","Large", false);
+
+        if (rbSmall.isChecked()) {
+            size = "small";
+            orderDescription = pizzaOrderSystem.OrderPizza(topping, size, true);
+        }
+        else if (rbMedium.isChecked()) {
+            size = "medium";
+            orderDescription = pizzaOrderSystem.OrderPizza(topping, size, false);
+        }
+        else if (rbLarge.isChecked()){
+            size = "large";
+            orderDescription = pizzaOrderSystem.OrderPizza(topping, size, true);
+        }
         //display a pop up message for a long period of time
         Toast.makeText(getApplicationContext(), "You have ordered a "+orderDescription , Toast.LENGTH_LONG).show();
         txtTotal.setText("Total Due: " + pizzaOrderSystem.getTotalBill().toString());
